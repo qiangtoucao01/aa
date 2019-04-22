73aa83cb7645cb1d206b9943f1b54320bcb79fa8
wangxun <1064981211@qq.com>
2019-04-22 17:54:27 +0800

    添加了脚本打包的功能。

--- Assets/Editor/JenkinsAdapter.cs
+++ Assets/Editor/JenkinsAdapter.cs
@@ -3,9 +3,17 @@ using UnityEditor;
 
 public  class JenkinsAdapter
 {
-   // [MenuItem("Jenkins/JenkinsBuild")] 
+    [MenuItem("Jenkins/JenkinsBuild")] 
     public static void Build() {
         EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android);
+
+      PlayerSettings.Android.keystoreName = @"E:\xiangmu\Jenkinsapk\user.keystore";
+        PlayerSettings.Android.keyaliasName = "userzs";
+        PlayerSettings.Android.keystorePass = "windowszuihao8";
+        PlayerSettings.Android.keyaliasPass = "windowszuihao8";
+
+       
+        
         List<string> sceneList = new List<string>();
         EditorBuildSettingsScene[] temp = EditorBuildSettings.scenes;
         for (int i = 0, iMax = temp.Length; i < iMax; ++i) sceneList.Add(temp[i].path);
